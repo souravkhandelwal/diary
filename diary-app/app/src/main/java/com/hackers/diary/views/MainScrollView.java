58e20c63f63671719ca533f06a18c330689ca182
souravkhandelwal <sourav@infoaxe.com>
2014-09-01 12:39:03 -0700

    New Andriod studio project

---
+++ diary-app/app/src/main/java/com/hackers/diary/views/MainScrollView.java
@@ -0,0 +1,14 @@
+package com.hackers.diary.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
+public class MainScrollView extends ScrollView {
+
+	public MainScrollView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+}
